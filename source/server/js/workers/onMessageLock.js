b54efda7a957a2bf6ecccdf8ae0d50e7358886a3
Federico Ghedina <fedeghe@gmail.com>
2018-06-06 23:53:08 +0200

    froceries

--- source/server/js/workers/onMessageLock.js
+++ source/server/js/workers/onMessageLock.js
@@ -1,10 +1,13 @@
 /*========================================================================================*/
 /*====*/
+/*====*/    /*-----------------------------------*/
 /*====*/    /* MANDATORY TO SET/CHECK collisions */
+/*====*/    /*-----------------------------------*/
 /*====*/
 /*====*/    var actors = null,
 /*====*/        enforceActorsMatch = $ACTORS.ENFORCE$,
 /*====*/        gotActorsDontMatch = null;
+/*====*/
 /*====*/    // this is for init settings of the actors,
 /*====*/    // from bro toward the worker
 /*====*/    if (e.data.___TYPE === '___INITACTORS') {
@@ -16,7 +19,11 @@
 /*====*/    // worker receives a message from the websocket
 /*====*/    if (gotActorsDontMatch || (enforceActorsMatch && !actors))
 /*====*/        throw 'Actors area required and are not provided OR does not match';
-/*====*/    // end of mandatory stuff, unlukily for the moment
-/*====*/    // is not possible to write it in the worker when created
+/*====*/
+/*====*/    /*--------------------------------------------------*/
+/*====*/    /* end of mandatory stuff, unlukily for the moment  */
+/*====*/    /* is not possible to write it in the worker when   */
+/*====*/    /* created                                          */
+/*====*/    /*--------------------------------------------------*/
 /*====*/
 /*========================================================================================*/
