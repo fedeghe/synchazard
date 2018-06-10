fd7f7d22d14409b8f728e6b8b24f8d19805ced4c
Federico Ghedina <fedeghe@gmail.com>
2018-06-10 21:55:54 +0200

    test ongoing

--- source/test/index.js
+++ source/test/index.js
@@ -9,8 +9,16 @@ describe('index', () => {
         const page = await browser.newPage();
         await page.goto(`${opts.appUrl}`);
         const checks = {
+            header: 1,
+            span: 1,
+            a: 2,
+            br: 1,
+            div: 2,
+            h2: 1,
+            sub: 1,
             img: 1,
-            h2:1
+            canvas: 1,
+            script: 2
         };
         let tmp = null;
         for (let tag in checks) {
@@ -21,9 +29,13 @@ describe('index', () => {
 
     it('data from json should match data on page', test(async (browser, opts) => {
         const page = await browser.newPage();
-        await page.goto(`${opts.appUrl}`);
-    
+        await page.goto(`${opts.appUrl}`);    
+
         let tmp = await page.$eval('h2', d => d.innerText);
         expect(tmp).to.be.equal(data.title + data.version);
+
+        tmp = await page.$eval('p', d => d.innerText);
+        expect(tmp).to.be.equal(data.content);
     }));
+    
 });
