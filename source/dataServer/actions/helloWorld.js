744d74a7b611b75ab8b8163a654d027774887db2
Federico Ghedina <fedeghe@gmail.com>
2018-06-17 15:18:22 +0200

    avoid indentation if possible

--- source/dataServer/actions/helloWorld.js
+++ source/dataServer/actions/helloWorld.js
@@ -10,28 +10,26 @@ module.exports.launch = (action, socketSrv, params) => {
     let state = {one: 0, two: 1};
 
     action.onconnection((data, ws) => {
-        
-        if (data.___TYPE === 'action') {
-            switch (data.___ACTION) {
-                case 'init':
-                    ws.send(action.encodeMessage({
-                        ___ACTION: 'initDone',
-                        ___PAYLOAD: state,
-                        ___TIME: data.___TIME
-                    }));
-                    break;
-                case 'next':
-                    updateState();
-                    socketSrv.broadcast(action.encodeMessage({
-                        ___ACTION: 'nextDone',
-                        ___PAYLOAD: state,
-                        ___TIME: data.___TIME
-                    }));
-                    ws.send(action.encodeMessage({
-                        ___ACTION: 'boldMe'
-                    }));
-                    break;
-            }
+        if (data.___TYPE !== 'action') return;
+        switch (data.___ACTION) {
+            case 'init':
+                ws.send(action.encodeMessage({
+                    ___ACTION: 'initDone',
+                    ___PAYLOAD: state,
+                    ___TIME: data.___TIME
+                }));
+                break;
+            case 'next':
+                updateState();
+                socketSrv.broadcast(action.encodeMessage({
+                    ___ACTION: 'nextDone',
+                    ___PAYLOAD: state,
+                    ___TIME: data.___TIME
+                }));
+                ws.send(action.encodeMessage({
+                    ___ACTION: 'boldMe'
+                }));
+                break;
         }
     });
 };
