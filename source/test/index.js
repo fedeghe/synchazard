1114d44669ea1fef75cc66f76b54b348bdf592a6
Federico Ghedina <fedeghe@gmail.com>
2018-06-17 14:58:48 +0200

    small refactor

--- source/test/index.js
+++ source/test/index.js
@@ -6,21 +6,22 @@ const data = require('./../dataServer/data1.json');
 
 describe('index', () => {
     it('should contain all used tags', test(async (browser, opts) => {
-        const page = await browser.newPage();
-        await page.goto(`${opts.appUrl}`);
-        const checks = {
-            header: 1,
-            span: 1,
-            a: 2,
-            br: 1,
-            div: 2,
-            h2: 1,
-            sub: 1,
-            img: 1,
-            canvas: 1,
-            script: 2
-        };
+        const page = await browser.newPage(),
+            checks = {
+                header: 1,
+                span: 1,
+                a: 2,
+                br: 1,
+                div: 2,
+                h2: 1,
+                sub: 1,
+                img: 1,
+                canvas: 1,
+                script: 2
+            };
         let tmp = null;
+
+        await page.goto(`${opts.appUrl}`);
         for (let tag in checks) {
             tmp = await page.$$eval(`body ${tag}`, d => d.length);
             expect(tmp).to.be.equal(checks[tag]);
