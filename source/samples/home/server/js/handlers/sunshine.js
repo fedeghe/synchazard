e643bf168cf43ea81ffb9351c68321fe64592fb7
Federico Ghedina <fedeghe@gmail.com>
2018-07-07 10:22:28 +0200

    not use ref removed

--- source/samples/home/server/js/handlers/sunshine.js
+++ source/samples/home/server/js/handlers/sunshine.js
@@ -1,7 +1,5 @@
 (function () {
     "use strict";
-    var trg = document.getElementById('number');
-    
     $NS$.handlers.sunshine = function (d) {
         // console.log(d);
         document.body.style.background= 'linear-gradient(to bottom,' + d.join(',') + ')';
