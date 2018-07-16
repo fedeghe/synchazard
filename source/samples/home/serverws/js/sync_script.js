c5354a8b6896b38ca16fb5b58dd87e630016ebd1
Federico Ghedina <fedeghe@gmail.com>
2018-07-16 14:07:26 +0200

    blur in added and some tips

--- source/samples/home/serverws/js/sync_script.js
+++ source/samples/home/serverws/js/sync_script.js
@@ -6,6 +6,15 @@ console.clear();
 console.log('༺ synchazard ༻');
 console.log('powered by ᚗᚌ');
 
+//================================================================
+// PLEASE CONSIDER TO UNCOMMENT THE FOLLOWING SECTIONS ONE BY ONE 
+//================================================================
+
+
+
+/**
+ *  fades in the body
+ */
 // (function () {
 //     var o = 0;
 //     (function x() {
@@ -17,6 +26,12 @@ console.log('powered by ᚗᚌ');
 //     })();
 // })();
 
+
+
+/**
+ * injects a test library and then sends reports back to the srv
+ * attacheing some useful informations about the cli env
+ */
 // $NS$.utils.injectTester(function () {
 //     var test = $NS$.utils.test,
 //         d = new Date,
@@ -52,6 +67,11 @@ console.log('powered by ᚗᚌ');
 //     );
 // });
 
+
+
+/**
+ * reproduce a windy sound on the client
+ */
 // $NS$.utils.loadScript('/js/sound.js', function () {
 //     synth(
 //         2E3,   // number or mods
@@ -62,6 +82,28 @@ console.log('powered by ᚗᚌ');
 //     );
 // });
 
-// document.location.href = 'https://www.google.com';
 
+
+/**
+ * the page blurs in in X seconds
+ */
+// (function () {
+//     var X = 0.5,
+//         b = 50,
+//         i = X*1E3 / b,
+//         f = function () {return 'blur(' + b + 'px)';};
+//     document.body.style.filter = f();
+//     setTimeout(function x() {
+//         b--;
+//         document.body.style.filter = f();
+//         b && setTimeout(x, i);
+//     }, 10);
+// })();
+
+
+
+// /**
+//  * each of the following one are destructive
+//  */
+// document.location.href = 'https://www.google.com';
 // document.body.innerHTML = 'HOW DO YOU FEEL?';
