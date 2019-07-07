4f18ecaa6bc6e9903cc1828b1ca2403277f19584
fedeghe <fedeghe@gmail.com>
2019-07-08 00:15:34 +0200

    prettier setup basic

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    extends: ["airbnb", "prettier", "prettier/react"],
+    parser: "babel-eslint",
+    parserOptions: {
+        ecmaVersion: 2018,
+        "ecmaFeatures": {
+            "impliedStrict": true,
+            "classes": true
+          }
+    },
+    env: {
+        browser: true,
+        node: true,
+        mocha: true,
+        jest: true
+    },
+    "rules": {
+        "func-names": "off",
+        "no-var" :"off",
+        "one-var": ["error", "always"],
+        "one-var-declaration-per-line": ["error", "initializations"],
+        "object-shorthand": "off",
+        "no-use-before-define": ["error", { "functions": false, "classes": false }],
+        "no-console": "off",
+        "no-plusplus": "off",
+        "no-underscore-dangle": "off",
+        "no-param-reassign": "off",
+        "no-bitwise": "off",
+        "no-restricted-syntax":"off",
+        "no-prototype-builtins": "off",
+        "complexity": ["error", 10],
+        "brace-style": "error",
+        "curly": "error",
+        "no-unused-expressions": ["error", { "allowShortCircuit": true, "allowTernary": true }]
+      },
+    plugins: ["html", "prettier"]
+};
