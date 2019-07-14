7d9301fe987d71a6667b3f4a44c10dab29ffe903
fedeghe <fedeghe@gmail.com>
2019-07-14 20:22:16 +0200

    lintz

--- source/samples/home/serverws/actions/doJob.js
+++ source/samples/home/serverws/actions/doJob.js
@@ -1,9 +1,8 @@
-module.exports.launch = (action, synchazard, params) => {
-    'use strict';
+module.exports.launch = (action /* , synchazard, params */) => {
     const jobs = {
         getFunc1: {
             func: function (r) {
-                return Math.pow(3, r);
+                return 3 ** r;
             },
             description: '3 ^ $p'
         }
@@ -12,15 +11,16 @@ module.exports.launch = (action, synchazard, params) => {
     action.onconnection((data, ws) => {
         if (data._TYPE !== 'action') return;
         switch (data._ACTION) {
-        case 'init':
-            ws.send(action.encodeMessage({
-                _ACTION: 'doComputation',
-                _JOB: {
-                    func: jobs.getFunc1.func.toString(),
-                    desc: jobs.getFunc1.description
-                }
-            }));
-            break;
+            case 'init':
+                ws.send(action.encodeMessage({
+                    _ACTION: 'doComputation',
+                    _JOB: {
+                        func: jobs.getFunc1.func.toString(),
+                        desc: jobs.getFunc1.description
+                    }
+                }));
+                break;
+            default: break;
         }
     });
 };
