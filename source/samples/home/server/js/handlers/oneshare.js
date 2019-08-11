4c2075a3e6ade6fae6750a884b13ea2c1af9c7ca
fedeghe <fedeghe@gmail.com>
2019-08-11 22:28:41 +0200

    dio

--- source/samples/home/server/js/handlers/oneshare.js
+++ source/samples/home/server/js/handlers/oneshare.js
@@ -1,7 +1,7 @@
 (function () {
     function OneShare() {}
     OneShare.prototype.handle = function (d) {
-        console.log(d
+        console.log(d);
     };
     // eslint-disable-next-line no-undef
     maltaV('NS').handlers.oneShare = new OneShare;
