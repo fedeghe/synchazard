7f1edea798752a82a7645caca6891f3c3a99e57b
fedeghe <fedeghe@gmail.com>
2019-08-13 07:18:55 +0200

    style

--- source/samples/home/server/js/oneshare.js
+++ source/samples/home/server/js/oneshare.js
@@ -2,5 +2,15 @@
  *
  */
 (function () {
-    maltaV('NS').utils.loadScript('/js/handlers/oneshare.js');
+
+    function createUi(trg) {
+        var shareArea = document.createElement('div'),
+            contentArea = document.createElement('div');
+    }
+
+    window.addEventListener('load', function () {
+        var target = document.getElementById('target');
+        createUi(target);
+        maltaV('NS').utils.loadScript('/js/handlers/oneshare.js');
+    });
 })();
