2c2cc084ac218f3f511771c3fe8b2731387121d6
fedeghe <fedeghe@gmail.com>
2019-08-13 23:53:19 +0200

    style should ebe ok

--- source/samples/home/server/js/oneshare.js
+++ source/samples/home/server/js/oneshare.js
@@ -3,10 +3,27 @@
  */
 (function () {
 
+
+    function createShareArea()  {}
+
     function createUi(trg) {
         var shareArea = document.createElement('div'),
-            contentArea = document.createElement('div');
+                shareAreaDrop = document.createElement('div'),
+                shareAreaButtons = document.createElement('div'),
+
+            contentArea = document.createElement('div'),
+                contentAreaSelect = document.createElement('div'),
+                contentAreaTab = document.createElement('div'),
+                contentAreaTabTongues = document.createElement('div'),
+                contentAreaTabContent = document.createElement('div'),
+
+         t = document.querySelector('.tabTongue');
+        t.addEventListener('click', function (e) {
+            console.log(e)
+        })
+
     }
+
 
     window.addEventListener('load', function () {
         var target = document.getElementById('target');
