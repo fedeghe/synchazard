21fbf6158ab660a5373e1cb36e7e856d1647ba98
fedeghe <fedeghe@gmail.com>
2019-08-21 00:38:42 +0200

    bit of description for ws answers

--- source/samples/home/serverws/actions/oneshare.js
+++ source/samples/home/serverws/actions/oneshare.js
@@ -13,12 +13,15 @@ module.exports.launch = (action, synchazard /* , params */) => {
             }]
         } */},
         actions: {
-            sendSharedFiles: userId => action.encode({
+            sendSharedFiles: () => action.encode({
                 _ACTION: 'userFiles',
                 _PAYLOAD: {
                     files: Object.keys(action.data.files)
-                        .filter(uk => uk !== userId).map(uk => ({
-                            [uk]: action.data.files[uk]
+                        .map(uk => ({
+                            [uk]: action.data.files[uk].map(f => ({
+                                filePath: f.filePath,
+                                subscribersCount: f.subscribers.length,
+                            }))
                         }))
                 }
             }),
@@ -26,7 +29,11 @@ module.exports.launch = (action, synchazard /* , params */) => {
                 if (!(userId in action.data.files)) {
                     action.data.files[userId] = []
                 }
-                action.data.files[userId].push({filePath, content});
+                action.data.files[userId].push({
+                    filePath,
+                    content,
+                    subscribers: []
+                });
                 return action.encode({
                     _ACTION: 'sharedAdded',
                     _PAYLOAD: {
@@ -74,20 +81,54 @@ module.exports.launch = (action, synchazard /* , params */) => {
     action.onconnection((data, ws) => {
         if (data._TYPE !== 'action') return;
         switch (data._ACTION) {
-            case 'init':
-                ws.send(action.data.actions.sendSharedFiles(data._ID));
-                break;
-            case 'addShare':
-                
-                break;
-            case 'removeShare':
 
+            /**
+             * when a client connects just
+             * - send the list of all shared files (the client will have to ignore his ones)
+             *  | but only {filePath, subscribersCount}
+             */
+            case 'init':
+                ws.send(action.data.actions.sendSharedFiles());
                 break;
+
+            /**
+             * when a user shares a file just 
+             * - if not existing, create the userId keyed array in action.data.files
+             * - add {filePath, content} to action.data.files[userId]
+             * - broadcast shared files (all clients will have to ignore their ones)
+             */
+            case 'addShare':
+                break;
+
+            /**
+             * when a user update a file the new content will be sent
+             * (the file is already in the users list)
+             * - update the entry in the userId keyed array
+             * - broadcast to all subscribers
+             */
+            case 'updateShare':
+                break;
+
+            /**
+             * when the user unshare a file just
+             * - remove it from the userId list
+             * - broadcast shared files (all clients will have to ignore their ones)
+             */
+            case 'removeShare':
+                break;
+
+            /**
+             * when a client subscribe to a file just
+             * - add his userId into the file's subscribers list
+             */
             case 'addShared':
-                
                 break;
+
+            /**
+             * when a client unsubscribe to a file just
+             * - remove his userId into the file's subscribers list
+             */
             case 'removeShared':
-                
                 break;
 
             default: break;
