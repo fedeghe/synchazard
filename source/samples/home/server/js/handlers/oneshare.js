70fbab032896f30225026bd148a09f3da1f182ea
Federico <federico.ghedina@signavio.com>
2019-08-26 08:23:49 +0200

    zzz

--- source/samples/home/server/js/handlers/oneshare.js
+++ source/samples/home/server/js/handlers/oneshare.js
@@ -9,7 +9,7 @@
     Share.prototype.handle = function (data) {
         console.log('cli gets ', data )
     }
-    console.log(oneShare)
+    // console.log(oneShare)
     oneShare.shareArea.onAdd = function () {console.log('share add', arguments)}
     oneShare.shareArea.onRemove = function () {console.log('share remove', arguments)}
     oneShare.sharedArea.onAdd = function () {console.log('shared add', arguments)}
