c5b7fc50f006d976be0c63a0d38983a4576ccb4f
fedeghe <fedeghe@gmail.com>
2019-09-09 22:48:06 +0200

    bookmarklet for charles

---
+++ source/tk.js
@@ -0,0 +1,7 @@
+(function replaceHostname(newHostname) {
+	var dl = document.location,
+  		proto = dl.protocol,
+      port = dl.port,
+      pathname = dl.pathname
+  document.location.replace(`${proto  }//${  newHostname  }:${  port  }${  pathname}`)
+})('localhost.charlesproxy.com')
