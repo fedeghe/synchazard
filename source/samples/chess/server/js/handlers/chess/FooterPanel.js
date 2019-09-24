e984fece9bb611009eda1e1014bb1fa775d83f7a
fedeghe <fedeghe@gmail.com>
2019-09-24 08:50:31 +0200

    containers in head/foot

--- source/samples/chess/server/js/handlers/chess/FooterPanel.js
+++ source/samples/chess/server/js/handlers/chess/FooterPanel.js
@@ -2,9 +2,12 @@
 /* eslint-disable no-undef */
 function FooterPanel () {
     this.container = CL.dom.create({ tag: 'div', cls: 'footerContainer' });
+    this.footer = CL.dom.create({ tag: 'div', cls: 'footer' });
+
     this.init();
 }
 
 FooterPanel.prototype.init = function () {
-    this.container.innerHTML = 'This is the footer';
+    this.footer.innerHTML = 'This is the footer';
+    this.container.appendChild(this.footer)
 };
--- source/samples/chess/server/js/handlers/chess/HeaderPanel.js
+++ source/samples/chess/server/js/handlers/chess/HeaderPanel.js
@@ -2,9 +2,12 @@
 /* eslint-disable no-undef */
 function HeaderPanel () {
     this.container = CL.dom.create({ tag: 'div', cls: 'headerContainer' });
+    this.header = CL.dom.create({ tag: 'div', cls: 'header' });
+
     this.startGameButton = CL.dom.create({ tag: 'button', cls: 'startButton' });
     this.startGameButton.innerHTML = 'StartMatch';
-    this.container.appendChild(this.startGameButton);
+    this.header.appendChild(this.startGameButton);
+    this.container.appendChild(this.header);
     this.startGameButton.addEventListener('click', function () {
         // send straight the action
         maltaV('NS').send({ _ACTION: 'createMatch' });
