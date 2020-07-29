aa515ef4de69e18ba522a4cbd88ac192cacb56d5
fedeghe <fedeghe@gmail.com>
2020-07-29 07:21:57 +0200

    space

--- source/tk.js
+++ source/tk.js
@@ -3,5 +3,5 @@
   		proto = dl.protocol,
       port = dl.port,
       pathname = dl.pathname
-  document.location.replace(`${proto  }//${  newHostname  }:${  port  }${  pathname}`)
+  document.location.replace(`${proto}//${newHostname}:${port}${pathname}`)
 })('localhost.charlesproxy.com')
